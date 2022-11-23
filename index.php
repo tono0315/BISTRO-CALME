fd416d2fb4c74e20d70146d867712ece51223318
tonokawaPrivate <115854335+tonokawaPrivate@users.noreply.github.com>
2022-11-23 15:21:17 +0900

    PageTopへの文言を戻しました

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         </div>
     </main>
 
-    <div class="pagetop js-pagetop"><i class="fas fa-angle-up"></i>PAGE TOPへ戻る</div>
+    <div class="pagetop js-pagetop"><i class="fas fa-angle-up"></i>PAGE TOP</div>
     <?php get_footer();?>
     
     </body>
